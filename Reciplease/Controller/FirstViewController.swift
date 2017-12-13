78f0a3eacbaca0a98215bb758ebb1f4171c54c96
jullianm <jllnmercier@gmail.com>
2017-12-13 08:18:26 +0100

    First commit

---
+++ Reciplease/Controller/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Reciplease
+//
+//  Created by jullianm on 13/12/2017.
+//  Copyright © 2017 jullianm. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
